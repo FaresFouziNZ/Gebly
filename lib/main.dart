513d25781c9740c51db6d435091dd08e29381982
FaresFouziNZ <74138397+FaresFouziNZ@users.noreply.github.com>
2023-12-05 22:08:14 +0300

    add config to main
    routerProvider and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gebly/constants/themes.dart';
+import 'package:gebly/core/providers/app_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'credentials.dart' as credentials;
+import 'constants/credentials.dart' as credentials;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,20 +11,21 @@ void main() async {
     url: credentials.url,
     anonKey: credentials.anonKey,
   );
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
+      theme: ThemeData(colorScheme: lightColorScheme, scaffoldBackgroundColor: const Color(0xFFFFEEDE)),
+      darkTheme: ThemeData(colorScheme: darkColorScheme),
+      themeMode: ThemeMode.system,
     );
   }
 }
