563171997200ff0cda7269317632051c62a268e7
FaresFouziNZ <74138397+FaresFouziNZ@users.noreply.github.com>
2023-12-06 16:50:48 +0300

    added drawer to home view

--- lib/presentation/view/home_view.dart
+++ lib/presentation/view/home_view.dart
@@ -12,26 +12,127 @@ class HomeView extends StatelessWidget {
         title: const Text('HomeView'),
       ),
       drawer: Drawer(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+        backgroundColor: const Color(0xfffef1f0),
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
+            SizedBox(
+              height: 120,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Image.asset('assets/images/Title.png'),
+                ],
               ),
-              child: Text('Drawer Header'),
             ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                Navigator.pop(context);
-              },
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ListTile(
+                minLeadingWidth: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                tileColor: const Color(0xffffffff),
+                leading: const Icon(Icons.home_outlined, color: Color(0xff000000)),
+                title: const Text('Home'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
             ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                Navigator.pop(context);
-              },
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ListTile(
+                minLeadingWidth: 0,
+                leading: const Icon(Icons.history, color: Color(0xff000000)),
+                title: const Text('Previous events'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ListTile(
+                minLeadingWidth: 0,
+                leading: const Icon(Icons.add_box_outlined, color: Color(0xff000000)),
+                title: const Text('Join event'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            const Divider(
+              color: Color(0x33000000),
+              thickness: 1,
+              indent: 15,
+              endIndent: 15,
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(25.0, 10, 0, 11),
+              child: Text('Settings', style: TextStyle(fontSize: 14, color: Color(0xff000000))),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ListTile(
+                minLeadingWidth: 0,
+                leading: const Icon(Icons.broken_image_outlined, color: Color(0xff000000)),
+                title: const Text('Theme'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ListTile(
+                minLeadingWidth: 0,
+                leading: const Icon(Icons.add_card_outlined, color: Color(0xff000000)),
+                title: const Text('Payment information'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            const Divider(
+              color: Color(0x33000000),
+              thickness: 1,
+              indent: 15,
+              endIndent: 15,
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(25.0, 10, 0, 11),
+              child: Text('About us', style: TextStyle(fontSize: 14, color: Color(0xff000000))),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ListTile(
+                minLeadingWidth: 0,
+                leading: const Icon(Icons.question_mark, color: Color(0xff000000)),
+                title: const Text('FAQ'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ListTile(
+                minLeadingWidth: 0,
+                leading: const Icon(Icons.message_outlined, color: Color(0xff000000)),
+                title: const Text('Contact us'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
             ),
           ],
         ),
@@ -68,55 +169,61 @@ class HomeView extends StatelessWidget {
                       )),
                 ),
               ),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.925,
-                height: MediaQuery.of(context).size.height * 0.19,
-                decoration: BoxDecoration(
-                  // color: Theme.of(context).colorScheme.secondaryContainer,
-                  image: const DecorationImage(
-                    image: AssetImage('assets/images/eventAd.png'),
-                    fit: BoxFit.fitWidth,
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.925,
+                  height: MediaQuery.of(context).size.height * 0.19,
+                  decoration: BoxDecoration(
+                    // color: Theme.of(context).colorScheme.secondaryContainer,
+                    image: const DecorationImage(
+                      image: AssetImage('assets/images/eventAd.png'),
+                      fit: BoxFit.fitWidth,
+                    ),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Theme.of(context).colorScheme.secondaryContainer,
+                        Theme.of(context).colorScheme.primaryContainer,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(7.0),
                   ),
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Theme.of(context).colorScheme.secondaryContainer,
-                      Theme.of(context).colorScheme.primaryContainer,
-                    ],
-                  ),
-                  borderRadius: BorderRadius.circular(7.0),
                 ),
               ),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.925,
-                height: MediaQuery.of(context).size.height * 0.19,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [Colors.transparent, Colors.black.withOpacity(0.8)],
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.925,
+                  height: MediaQuery.of(context).size.height * 0.19,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.transparent, Colors.black.withOpacity(0.8)],
+                    ),
+                    borderRadius: BorderRadius.circular(7.0),
                   ),
-                  borderRadius: BorderRadius.circular(7.0),
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.only(left: 8.0, bottom: 8.0, right: 8.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Gathering? \nStart here',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 8.0, bottom: 8.0, right: 8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Gathering? \nStart here',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                          ),
                         ),
-                      ),
-                      Icon(
-                        Icons.arrow_forward,
-                        color: Colors.white,
-                      )
-                    ],
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
