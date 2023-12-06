c50b138dc900b10e9eece1b18d7bc45c84f92c78
FaresFouziNZ <74138397+FaresFouziNZ@users.noreply.github.com>
2023-12-07 01:05:16 +0300

    Get User Active Event ID Function

--- lib/core/services/database_queries.dart
+++ lib/core/services/database_queries.dart
@@ -1,15 +1,20 @@
+import 'package:gebly/core/models/event.dart';
+import 'package:gebly/core/models/item.dart';
+import 'package:gebly/core/models/order.dart';
 import 'package:gebly/core/services/database_tables.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class DatabaseQueries {
-  // DatabaseQueries(this.ref);
-  // final ProviderContainer ref;
   final DatabaseTables _tables = DatabaseTables();
   final auth = Supabase.instance.client.auth;
-  Future getUserActiveEvent() async {
-    //FIXME
+
+  Future<String> getUserActiveEventID() async {
     final userID = auth.currentUser!.id;
-    var activeEventID = await _tables.users.select('SELECT active_event').eq('id', userID);
-    return activeEventID;
+    var userInfo = await _tables.users.select().eq('id', userID);
+    if (userInfo[0]['active_event_id'] == null) {
+      throw Exception('User not in event');
+    }
+    return userInfo[0]['active_event_id'];
+  }
   }
 }
