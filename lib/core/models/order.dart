e3885611a4266eedf5ce7d157bec987e35564656
FaresFouziNZ <74138397+FaresFouziNZ@users.noreply.github.com>
2023-12-06 22:28:16 +0300

    add active event attribute

--- lib/core/models/order.dart
+++ lib/core/models/order.dart
@@ -3,6 +3,7 @@ class Order {
   final String userID;
   final int eventID;
   final int total;
+  final String activeEventID;
   final DateTime createdAt;
 
   Order({
@@ -10,6 +11,7 @@ class Order {
     required this.userID,
     required this.eventID,
     required this.total,
+    required this.activeEventID,
     required this.createdAt,
   });
 
@@ -19,6 +21,7 @@ class Order {
       userID: json['user_id'],
       eventID: json['event_id'],
       total: json['total'],
+      activeEventID: json['active_event_id'],
       createdAt: DateTime.parse(json['created_at']),
     );
   }
@@ -29,6 +32,7 @@ class Order {
       'user_id': userID,
       'event_id': eventID,
       'total': total,
+      'active_event_id': activeEventID,
       'created_at': createdAt.toIso8601String(),
     };
   }
@@ -38,11 +42,12 @@ class Order {
         userID == other.userID &&
         eventID == other.eventID &&
         total == other.total &&
+        activeEventID == other.activeEventID &&
         createdAt == other.createdAt;
   }
 
   @override
   String toString() {
-    return 'Order(orderID: $orderID, userID: $userID, eventID: $eventID, total: $total, createdAt: $createdAt)';
+    return 'Order{order_id: $orderID, user_id: $userID, event_id: $eventID, total: $total, active_event_id: $activeEventID, created_at: $createdAt}';
   }
 }
