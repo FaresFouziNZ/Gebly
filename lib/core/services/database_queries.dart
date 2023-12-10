ea51c2733a753898d70752d143812ad60d9ade86
FaresFouziNZ <74138397+FaresFouziNZ@users.noreply.github.com>
2023-12-10 20:36:05 +0300

    added base database queries

--- lib/core/services/database_queries.dart
+++ lib/core/services/database_queries.dart
@@ -1,6 +1,7 @@
 import 'package:gebly/core/models/event.dart';
 import 'package:gebly/core/models/item.dart';
 import 'package:gebly/core/models/order.dart';
+import 'package:gebly/core/models/restaurant.dart';
 import 'package:gebly/core/services/database_tables.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -17,8 +18,10 @@ class DatabaseQueries {
     return userInfo[0]['active_event_id'];
   }
 
-  Future<bool> isUserFree() async {
-    final userID = auth.currentUser!.id;
+  Future<bool> isUserFree({String userID = ''}) async {
+    if (userID == '') {
+      userID = auth.currentUser!.id;
+    }
     var userInfo = await _tables.users.select().eq('id', userID);
     if (userInfo[0]['active_event_id'] == null) {
       return true;
@@ -26,13 +29,26 @@ class DatabaseQueries {
     return false;
   }
 
-  Future joinEventUsingCode({required String eventID}) async {
+  Future<bool> createEvent({required Event event}) async {
+    final response = await _tables.event.insert(event.toJson());
+    if (response.error != null) {
+      throw Exception('Error creating event');
+    } else {
+      return true;
+    }
+  }
+
+  Future<bool> joinEventByID({required String eventID}) async {
     final userID = auth.currentUser!.id;
     var userInfo = await _tables.users.select().eq('id', userID);
     if (userInfo[0]['active_event_id'] != null) {
       throw Exception('User already in event');
     }
-    _tables.users.update({'active_event_id': eventID}).eq('id', userID);
+    final response = await _tables.users.update({'active_event_id': eventID}).eq('id', userID);
+    if (response.error != null) {
+      throw Exception('Error joining event');
+    }
+    return true;
   }
 
   Future<Event> getEventByCode({required String code}) async {
@@ -43,9 +59,22 @@ class DatabaseQueries {
     return Event.fromJson(eventInfo[0]);
   }
 
-  Future getNearRestaurants() async {
+  Future<List<Restaurant>> getNearRestaurants() async {
     //FIXME: get user location
-    var restaurants = await _tables.restaurant.select().limit(10);
+    List<Restaurant> restaurants = [];
+    var restaurantsInfo = await _tables.restaurant.select().limit(10);
+    for (var i = 0; i < restaurantsInfo.length; i++) {
+      restaurants.add(Restaurant.fromJson(restaurantsInfo[i]));
+    }
+    return restaurants;
+  }
+
+  Future<List<Restaurant>> searchRestaurant({required String searchQuery}) async {
+    List<Restaurant> restaurants = [];
+    var restaurantsInfo = await _tables.restaurant.select().ilike('name', '%$searchQuery%').limit(10);
+    for (var i = 0; i < restaurantsInfo.length; i++) {
+      restaurants.add(Restaurant.fromJson(restaurantsInfo[i]));
+    }
     return restaurants;
   }
 
@@ -58,9 +87,14 @@ class DatabaseQueries {
     return menu;
   }
 
-  Future makeOrder({required Order order, required List<Item> items}) async {
+  Future<String> makeOrder({required Order order, required List<Item> items}) async {
     final newOrderID = await _tables.order.insert(order.toJson()).select();
     await addItemsToOrderItemTable(orderID: newOrderID, items: items);
+    return newOrderID;
+  }
+
+  Future setDeadline({required String eventID, required String time}) async {
+    await _tables.event.update({'deadline': time}).eq('id', eventID);
   }
 
   Future addItemsToOrderItemTable({required String orderID, required List<Item> items}) async {
@@ -68,6 +102,17 @@ class DatabaseQueries {
       _tables.orderItem.insert({'order_id': orderID, 'item_id': items[i].id});
     }
   }
-  //add item to order
-  //get order
+
+  Future<List<Order>> getOrders({required String eventID}) async {
+    List<Order> orders = [];
+    var ordersInfo = await _tables.order.select().eq('event_id', eventID);
+    for (var i = 0; i < ordersInfo.length; i++) {
+      orders.add(Order.fromJson(ordersInfo[i]));
+    }
+    return orders;
+  }
+
+  Stream getParticipationStatusStream({required String eventID}) {
+    return _tables.order.stream(primaryKey: ['order_id']).eq('event_id', eventID);
+  }
 }
